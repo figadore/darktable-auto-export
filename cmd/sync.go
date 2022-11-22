a0e94dd5e06b5f5ac5b14af1d7007c6fe51e9ddf
Reese Wilson <3253971+figadore@users.noreply.github.com>
2022-11-22 16:30:13 -0500

    add db unlock command
    prereq for #4

--- cmd/sync.go
+++ cmd/sync.go
@@ -30,7 +30,7 @@ to quickly create a Cobra application.`,
 	RunE: sync,
 }
 
-var opts options
+var syncOpts syncOptions
 
 func init() {
 	rootCmd.AddCommand(syncCmd)
@@ -42,13 +42,13 @@ func init() {
 	// syncCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Local flags which will only run when this command is called directly
-	syncCmd.Flags().StringVarP(&opts.inputFolder, "in", "i", "./", "Directory to look for raw image files")
-	syncCmd.Flags().StringVarP(&opts.outputFolder, "out", "o", "./", "Directory to export jpgs to")
-	syncCmd.Flags().StringVarP(&opts.command, "command", "c", "flatpak run --command=darktable-cli org.darktable.Darktable", "Darktable command or binary")
-	syncCmd.Flags().StringVarP(&opts.extension, "extension", "e", ".ARW", "Extension of raw files")
+	syncCmd.Flags().StringVarP(&syncOpts.inputFolder, "in", "i", "./", "Directory to look for raw image files")
+	syncCmd.Flags().StringVarP(&syncOpts.outputFolder, "out", "o", "./", "Directory to export jpgs to")
+	syncCmd.Flags().StringVarP(&syncOpts.command, "command", "c", "flatpak run --command=darktable-cli org.darktable.Darktable", "Darktable command or binary")
+	syncCmd.Flags().StringVarP(&syncOpts.extension, "extension", "e", ".ARW", "Extension of raw files")
 }
 
-type options struct {
+type syncOptions struct {
 	inputFolder  string
 	outputFolder string
 	extension    string
@@ -77,17 +77,17 @@ func parseConfig() Config {
 func sync(cmd *cobra.Command, args []string) error {
 	fmt.Println("sync called")
 	// Recurse through input directory
-	raws := sidecars.FindFilesWithExt(opts.inputFolder, opts.extension)
+	raws := sidecars.FindFilesWithExt(syncOpts.inputFolder, syncOpts.extension)
 	config := parseConfig()
 	for _, raw := range raws {
 		fmt.Println(raw)
 		// Find adjacent xmp files
 		xmps := sidecars.FindXmps(raw)
 		basename := strings.TrimSuffix(filepath.Base(raw), filepath.Ext(raw))
-		relativeDir := strings.TrimPrefix(filepath.Dir(raw), opts.inputFolder)
-		outputPath := filepath.Join(opts.outputFolder, relativeDir, fmt.Sprintf("%s.jpg", basename))
+		relativeDir := strings.TrimPrefix(filepath.Dir(raw), syncOpts.inputFolder)
+		outputPath := filepath.Join(syncOpts.outputFolder, relativeDir, fmt.Sprintf("%s.jpg", basename))
 		params := darktable.ExportParams{
-			Command:    opts.command,
+			Command:    syncOpts.command,
 			RawPath:    raw,
 			OutputPath: outputPath,
 		}
@@ -99,8 +99,8 @@ func sync(cmd *cobra.Command, args []string) error {
 				fmt.Println("  ", xmp)
 				// Export the RAW file
 				params.XmpPath = xmp
-				jpgFilename := sidecars.GetJpgFilename(xmp, opts.extension)
-				outputPath, err := filepath.Abs(filepath.Join(opts.outputFolder, relativeDir, jpgFilename))
+				jpgFilename := sidecars.GetJpgFilename(xmp, syncOpts.extension)
+				outputPath, err := filepath.Abs(filepath.Join(syncOpts.outputFolder, relativeDir, jpgFilename))
 				if err != nil {
 					log.Fatalf("Error getting jpg path: %v", err)
 				}
@@ -112,8 +112,8 @@ func sync(cmd *cobra.Command, args []string) error {
 	// Delete jpgs for missing raws
 	if config.DeleteMissing == "true" {
 		fmt.Println("Deleting jpgs for missing raws")
-		jpgs := sidecars.FindFilesWithExt(opts.outputFolder, ".jpg")
-		deleteJpgs(sidecars.FindJpgsWithoutRaw(jpgs, opts.inputFolder, opts.outputFolder, opts.extension))
+		jpgs := sidecars.FindFilesWithExt(syncOpts.outputFolder, ".jpg")
+		deleteJpgs(sidecars.FindJpgsWithoutRaw(jpgs, syncOpts.inputFolder, syncOpts.outputFolder, syncOpts.extension))
 		fmt.Printf("Found %v jpgs", len(jpgs))
 	} else {
 		fmt.Printf("Not deleting jpgs for missing raws: %s", config.DeleteMissing)
---
+++ cmd/unlock.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// unlockCmd represents the unlock command
+var unlockCmd = &cobra.Command{
+	Use:   "unlock",
+	Short: "Clear darktable db lock files",
+	Long: `Only clear the darktable db lock files if darktable
+is definitely not in use.`,
+	RunE: Unlock,
+}
+
+func init() {
+	rootCmd.AddCommand(unlockCmd)
+	unlockCmd.Flags().StringVarP(&unlockOpts.lockDir, "lockdir", "", "~/.var/app/org.darktable.Darktable/config/darktable/", "Directory where darktable lock files are kept. Often ~/.config/darktable for local installations")
+}
+
+var unlockOpts unlockOptions
+
+type unlockOptions struct {
+	lockDir string
+}
+
+func Unlock(cmd *cobra.Command, args []string) error {
+	fmt.Println("Deleting lock files")
+
+	dbLockPath := filepath.Join(unlockOpts.lockDir, "data.db.lock")
+	libraryLockPath := filepath.Join(unlockOpts.lockDir, "library.db.lock")
+	err := deleteFile(dbLockPath)
+	if err != nil {
+		return err
+	}
+	err = deleteFile(libraryLockPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteFile(path string) error {
+	if _, err := os.Stat(path); err == nil {
+		fmt.Printf("Removing %s\n", path)
+		e := os.Remove(path)
+		if e != nil {
+			return e
+		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		// file doesn't exist, nothing to delete
+		fmt.Println("No file found at", path)
+		return nil
+	} else {
+		return err
+	}
+	return nil
+}
