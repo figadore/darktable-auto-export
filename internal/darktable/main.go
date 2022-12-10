f496d43e978c4913428eb665cb50e33957f82673
Reese Wilson <3253971+figadore@users.noreply.github.com>
2022-12-09 21:33:55 -0500

    replace target jpg without deleting it first

--- internal/darktable/main.go
+++ internal/darktable/main.go
@@ -3,12 +3,9 @@ package darktable
 import (
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/figadore/darktable-auto-export/internal/sidecars"
 )
 
 type ExportParams struct {
@@ -30,17 +27,19 @@ func Export(params ExportParams) error {
 			return e
 		}
 	}
-	err := sidecars.DeleteJpgIfExists(params.OutputPath)
-	if err != nil {
-		log.Fatalf("Error deleting jpg: %v", err)
-	}
+	//err := sidecars.DeleteJpgIfExists(params.OutputPath)
+	//if err != nil {
+	//	log.Fatalf("Error deleting jpg: %v", err)
+	//}
 	//cmd := exec.Command("echo", params.rawPath, ":", params.xmpPath, "->", params.OutputPath)
 	args := strings.Fields(params.Command)
 	args = append(args, params.RawPath)
 	if params.XmpPath != "" {
 		args = append(args, params.XmpPath)
 	}
-	args = append(args, params.OutputPath)
+	//args = append(args, params.OutputPath)
+	tmpPath := fmt.Sprintf("%s.tmp.jpg", params.OutputPath)
+	args = append(args, tmpPath)
 	remaining := args[1:]
 	//cmd := exec.Command("echo", remaining...)
 	fmt.Println(args)
@@ -53,6 +52,11 @@ func Export(params ExportParams) error {
 	if err != nil {
 		fmt.Println("error", err.Error())
 		fmt.Println("err", err)
+		return err
+	}
+	// Move tmp file to target OutputPath. This is what allows an album to stay intact
+	err = os.Rename(tmpPath, params.OutputPath)
+	if err != nil {
 		return err
 	}
 	return nil
