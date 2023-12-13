cc273a3126a94128d244d43b6563295804df0b2a
Reese Wilson <3253971+figadore@users.noreply.github.com>
2023-12-12 20:44:42 -0800

    undo previous fix, probably still broken

--- internal/darktable/main.go
+++ internal/darktable/main.go
@@ -40,7 +40,7 @@ func Export(params ExportParams) error {
 	}
 	// Write to tmp file since darktable cli creates a new file if target
 	// filename exists
-	tmpPath := fmt.Sprintf("\"%s.tmp.jpg\"", params.OutputPath)
+	tmpPath := fmt.Sprintf("%s.tmp.jpg", params.OutputPath)
 	args = append(args, tmpPath)
 	err = runCmd(args, params.DryRun, true)
 	if err != nil {
